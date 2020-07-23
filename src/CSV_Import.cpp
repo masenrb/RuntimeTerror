6e7390db936aeb483cf1404a865c07c4d13e9d4b
Michel Chalfoun <mikachalfoun@gmail.com>
2020-07-22 22:51:19 -0400

    Fixed CSV Reading issue

--- src/CSV_Import.cpp
+++ src/CSV_Import.cpp
@@ -20,35 +20,75 @@ vector<DrugData> csvToMap(string filename)
     vector<DrugData> final;
     string line, curData;
 
-    // first makes sure to update prescription in the first iteration of the string stream bellow.
-    bool prescription, first;       
+    // Counter lets us know which data value we are currenlty on.
+    bool prescription;
+    int counter;
 
     // Reads every line in the CSV file.
     while (getline(dataFile, line))
     {
+        // Sets up variables we will be needing to store the data.
         stringstream lineStream(line);
-        
-        // Will store drug data required for the addData function.
-        vector<string> lineData;
         DrugData drug;
 
+        // Resets on every loop.
+        counter = 1;
+
         // Reads every element of the CSV line.
-        first = true;
         while (getline(lineStream, curData, ','))
         {
-            if(first){
-                if (curData == "HUMAN PRESCRIPTION DRUG"){
-                    prescription = true;
-                }else{
-                    prescription = false;
+            switch (counter)
+            {
+            // Handles Prescription value.
+            case 1:
+            {
+                if (curData == "HUMAN PRESCRIPTION DRUG")
+                {
+                    drug.updatePrescription(true);
                 }
-                first = false;
-            }else{
-                lineData.push_back(curData);
+                else
+                {
+                    drug.updatePrescription(false);
+                }
+                break;
             }
+            // Handles Proprietary Name.
+            case 2:
+            {
+                drug.update_P_Name(curData);
+                break;
+            }
+            // Handles Non Proprietary Name.
+            case 3:
+            {
+                drug.update_NP_Name(curData);
+                break;
+            }
+            // Handles all Dosage Forms.
+            case 4:
+            {
+                stringstream dosageStream(curData);
+                string curDosage;
+                while (getline(dosageStream, curDosage, '-'))
+                {
+                    drug.addDosageForm(curDosage);
+                }
+                break;
+            }
+            // Handles all Routes.
+            case 5:
+            {
+                stringstream routeStream(curData);
+                string curRoute;
+                while (getline(routeStream, curRoute, '-'))
+                {
+                    drug.addRoute(curRoute);
+                }
+                break;
+            }
+            }
+            counter++;
         }
-
-        drug.addData(lineData, prescription);
         final.push_back(drug);
     }
     return final;
@@ -63,6 +103,7 @@ int main()
     for (int i = 0; i < output.size(); i++)
     {
         output[i].printDrug();
+        cout << endl;
     }
     return 0;
 };
