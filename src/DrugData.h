98a285c5d806a9af646daac45a16af27d6c2caf6
Michel Chalfoun <mikachalfoun@gmail.com>
2020-07-27 22:19:31 -0400

    Added provider data handling and removed mutators

--- src/DrugData.h
+++ src/DrugData.h
@@ -5,9 +5,9 @@ using namespace std;
 //Header for the class that holds drug name along with additional drug data.
 struct DrugData
 {
-    string pName;                   //Proprietary name
+    string pName;                    //Proprietary name
     vector<string> npNames;          //Non-Proprietary names
-    bool prescription;              //Requires Prescription
+    bool prescription;               //Requires Prescription
     vector<string> forms;            //Dosage Form
     vector<string> routes;           //Routes
     vector<string> providers;        //Approved Producers
